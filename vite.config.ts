b30572d0133e91b01bb2f38fa5e0b8770ddf4132
Bekham254 <alexkoech325@gmail.com>
2025-07-15 16:31:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/globalc/',
-  plugins: [react()],
-});
+  base: '/globalc/', // Add this line
+  // ...other config
+})
